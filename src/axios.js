f9422899ef9e50fdb72137c2383d918ed7805bbe
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2022-04-21 08:19:52 +0200

    hello

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 const instance = axios.create({
-    baseURL: 'https://tinderreactbackend.herokuapp.com'
+    baseURL: 'https://tinderreactbackend.herokuapp.com/'
 });
 export default instance;
